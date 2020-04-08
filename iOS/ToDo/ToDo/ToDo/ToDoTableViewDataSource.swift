0ea7bca2d3b0596277c93e8e1545a29e8e7964ce
GangWoon <gangwoon2018@gmail.com>
2020-04-08 20:48:28 +0900

    chore: cell 데이터 처리
    임시 데이터를 사용해서 UI가 제대로 표시되는지 확인하는 작업을 진행했습니다.
    셀부터 등록되기떄문에 노티를 데이터소스에서 먼저 쏘도록 작성했습니다.

---
+++ iOS/ToDo/ToDo/ToDo/ToDoTableViewDataSource.swift
@@ -0,0 +1,37 @@
+//
+//  ToDoTableViewDataSource.swift
+//  ToDo
+//
+//  Created by Cloud on 2020/04/08.
+//  Copyright © 2020 Cloud. All rights reserved.
+//
+
+import UIKit
+
+extension Notification.Name {
+    static var pushCellCount: Notification.Name = Notification.Name(rawValue: "pushCellCount")
+}
+
+class ToDoTableViewDataSource: NSObject {
+    var tempData: [(title: String,description: String)] = [("할일","공부하기"),
+                                                           ("4월2일", "오토레이아웃자성하기\n유아이 작성하기\n버튼 추가"),
+                                                           ("4월3일", "DragAndDrop\nNSItem\nTableView\nTableViewCell")] {
+        didSet {
+            NotificationCenter.default
+                .post(name: Notification.Name.pushCellCount, object: nil, userInfo: ["count": tempData.count])
+        }
+    }
+}
+
+extension ToDoTableViewDataSource: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tempData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: ContainerTableViewCell.identifier, for: indexPath) as? ContainerTableViewCell else { return UITableViewCell() }
+        let item = tempData[indexPath.item]
+        cell.update(title: item.title, description: item.description)
+        return cell
+    }
+}
