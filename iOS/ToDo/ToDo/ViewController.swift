26fa3d0ff7d00f12a6c6640bf5d966b9fa8953b3
GangWoon <gangwoon2018@gmail.com>
2020-04-08 20:47:13 +0900

    chore: segue에 따른 데이터소스 등록
    세그에따라서 데이터소스를 다르게 지정해줄 생각입니다. 우선 todo만 작성했습니다.
    여기서 궁금한점이 UITableViewController를 생성하고 delegate, datasource를 다른 객체에게 위임을 하는 행동이 이상한건지 궁금합니다.

--- iOS/ToDo/ToDo/ViewController.swift
+++ iOS/ToDo/ToDo/ViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var todoTableViewDataSource = ToDoTableViewDataSource()
+    var todoTableViewDelegate = ToDoTableViewDelegate()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toDo" {
+            guard let toDoViewController = segue.destination as? ContainerTableViewController else { return }
+            toDoViewController.tableView.dataSource = todoTableViewDataSource
+            toDoViewController.tableView.delegate = todoTableViewDelegate
+        }
+    }
 }
-
