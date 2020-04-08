6420ab904208f8a6dd1da0dfa2288caaf70d2666
GangWoon <gangwoon2018@gmail.com>
2020-04-08 20:41:36 +0900

    feat: ContainerTableSectionHeaderView 생성
    컨테이너 테이블뷰컨트롤에서 사용할 섹션해더를 생성했습니다.
    아직 새로운 뷰컨트롤러를 작업하지 않아서 objc 메소드는 비어둔 상태입니다.

---
+++ iOS/ToDo/ToDo/ContainerViewController/ContainerTableSectionHeaderView.swift
@@ -0,0 +1,92 @@
+//
+//  ToDoSectionHeaderView.swift
+//  ToDo
+//
+//  Created by Cloud on 2020/04/07.
+//  Copyright © 2020 Cloud. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+
+class ContainerTableSectionHeaderView: UITableViewHeaderFooterView {
+    
+    static var height: CGFloat = 50
+    static var identifier: String = "ContainerTableSectionHeaderView"
+    private var countingLabel: UILabel!
+    private var descriptionLabel: UILabel!
+    private var addCellButton: UIButton!
+    
+    override init(reuseIdentifier: String?) {
+        super.init(reuseIdentifier: reuseIdentifier)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        configure()
+    }
+    
+    func updateCountingLabel(count: String, title: String) {
+        countingLabel.text = count
+        descriptionLabel.text = title
+    }
+    
+    private func configure() {
+        configureCountingLabel()
+        configureDescriptionLabe()
+        configureAddCellButton()
+    }
+    
+    private func configureCountingLabel() {
+        countingLabel = UILabel()
+        countingLabel.textAlignment = .center
+        countingLabel.layer.masksToBounds = true
+        countingLabel.layer.cornerRadius = 17
+        addSubview(countingLabel)
+        countingLabel.backgroundColor = .white
+        countingLabelMakeConstraint()
+    }
+    
+    private func configureDescriptionLabe() {
+        descriptionLabel = UILabel()
+        addSubview(descriptionLabel)
+        titleLabelMakeConstraint()
+    }
+    
+    private func configureAddCellButton() {
+        addCellButton = UIButton()
+        addCellButton.setImage(UIImage(systemName: "plus"), for: .normal)
+        addCellButton.tintColor = .darkGray
+        addSubview(addCellButton)
+        addCellButtonMakeConstraint()
+        addCellButton.addTarget(self, action: #selector(addCellAction), for: .touchUpInside)
+    }
+    
+    private func countingLabelMakeConstraint() {
+        countingLabel.snp.makeConstraints { make in
+            make.width.equalTo(countingLabel.snp.height)
+            make.leading.equalToSuperview().inset(12)
+            make.top.bottom.equalToSuperview().inset(8)
+        }
+    }
+    
+    private func titleLabelMakeConstraint() {
+        descriptionLabel.snp.makeConstraints { make in
+            make.leading.equalTo(countingLabel.snp.trailing).offset(8)
+            make.top.bottom.equalToSuperview().inset(8)
+        }
+    }
+    
+    private func addCellButtonMakeConstraint() {
+        addCellButton.snp.makeConstraints { make in
+            make.trailing.equalToSuperview().inset(8)
+            make.top.bottom.equalToSuperview().inset(8)
+            make.width.equalTo(addCellButton.snp.height)
+        }
+    }
+    
+    @objc func addCellAction() {
+        // MARK 뷰 모달 기능 추가
+    }
+}
