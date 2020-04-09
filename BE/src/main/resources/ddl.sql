991175f42bab5b84d116f279017e823904fe904a
kyungrae <rlarudfo93@gmail.com>
2020-04-09 12:04:25 +0900

    Feat: Root aggregate인 project fetch api 구현 - BE [#21]
    확장성을 위해 category에서 project로 aggregate root를 이동했다.

--- BE/src/main/resources/ddl.sql
+++ BE/src/main/resources/ddl.sql
@@ -1,20 +1,22 @@
-DROP TABLE IF EXISTS Card, Activity, Category;
+DROP TABLE IF EXISTS card, category, project;
 
-CREATE TABLE Category(
-    Id int auto_increment primary key ,
-    Title varchar(64) not null
+CREATE TABLE project (
+    id int auto_increment primary key,
+    title varchar(64) not null
 );
 
-CREATE TABLE Card(
-    Id int auto_increment primary key ,
-    Contents varchar(64) not null,
-    CategoryId int,
-    PrevCardId int,
-    FOREIGN KEY (CategoryId) REFERENCES  Category(Id) ON UPDATE CASCADE,
-    FOREIGN KEY (PrevCardId) REFERENCES Card(Id) ON UPDATE SET NULL
+CREATE TABLE category(
+    id int auto_increment primary key,
+    title varchar(64) not null,
+    project int,
+    project_key int,
+    FOREIGN KEY (project) REFERENCES project(Id) ON UPDATE CASCADE
 );
 
-CREATE TABLE Activity (
-    Id int auto_increment primary key ,
-    Contents varchar(64) not null
+CREATE TABLE card(
+    id int auto_increment primary key,
+    contents varchar(64) not null,
+    category int,
+    category_key int,
+    FOREIGN KEY (category) REFERENCES category(Id) ON UPDATE CASCADE
 );
