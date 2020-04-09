a2c66b2b2842ea34aa123ea685b63631eff1a044
suhyunsim <cnc1106@naver.com>
2020-04-09 17:06:38 +0900

    Feat: Cors 설정 추가

---
+++ BE/src/main/java/todo7/BE/config/CorsConfig.java
@@ -0,0 +1,19 @@
+package todo7.BE.config;
+
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*")
+                .allowedOrigins("*")
+                .allowedMethods(
+                        HttpMethod.GET.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.PATCH.name(),
+                        HttpMethod.DELETE.name());
+    }
+}
