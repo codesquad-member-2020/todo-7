3d54e5867c38c0852fe16f024c8e8d59eea3af29
GangWoon <gangwoon2018@gmail.com>
2020-04-09 12:01:45 +0900

    chore: 노티피케이션 확장
    사용되는 노티피케이션 네임을 한 장소에 모았습니다.

---
+++ iOS/ToDo/ToDo/Notification/Name.swift
@@ -0,0 +1,14 @@
+//
+//  Name.swift
+//  ToDo
+//
+//  Created by Cloud on 2020/04/09.
+//  Copyright © 2020 Cloud. All rights reserved.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static var newCard: Notification.Name = Notification.Name(rawValue: "newCard")
+    static var pushCellCount: Notification.Name = Notification.Name(rawValue: "pushCellCount")
+}
