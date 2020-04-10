61d0fefb20b76314fec83c2d046aeb244ceefe14
suhyunsim <cnc1106@naver.com>
2020-04-10 12:11:43 +0900

    Refactor: front 테스트 환경 localhost:5500 적용

--- BE/src/main/java/todo7/BE/config/CorsConfig.java
+++ BE/src/main/java/todo7/BE/config/CorsConfig.java
@@ -7,8 +7,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("*")
-                .allowedOrigins("*")
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5500")
                 .allowedMethods(
                         HttpMethod.GET.name(),
                         HttpMethod.POST.name(),
