8c7583381cf318e9e9085c386b958cb92c97d81a
kyungrae <rlarudfo93@gmail.com>
2020-04-13 17:14:23 +0900

    Fix: profile을 통해 개발환경 구분 설정 수정 [#32]
    profile annotation target 값이 method, type이다. 따라서 메소드 단위로 환경설정이 가능할지 알았지만 메소드의 경우 Bean annotation method 단위로 가능했다. 따라서 DevWebConfig.java, DeployWebConfig.java CommonWebConfig.java 파일로 구별하기로 결정했다.

--- BE/src/main/java/todo7/BE/config/DevWebConfig.java
+++ BE/src/main/java/todo7/BE/config/DevWebConfig.java
@@ -5,12 +5,13 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Profile("dev")
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class DevWebConfig implements WebMvcConfigurer {
 
-    @Profile("dev")
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+        System.out.println("dev");
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:5500")
                 .allowedMethods("*");
