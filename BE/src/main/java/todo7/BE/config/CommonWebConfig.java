c1d7a0f6ff3cc9b5c4c3b4b44c86e54a62caece9
suhyunsim <cnc1106@naver.com>
2020-04-14 20:22:17 +0900

    Refactor: CommonWebConfig에 인터셉터, jwt과 관련된 Bean 설정

--- BE/src/main/java/todo7/BE/config/CommonWebConfig.java
+++ BE/src/main/java/todo7/BE/config/CommonWebConfig.java
@@ -5,21 +5,26 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import todo7.BE.interceptor.AuthCheckInterceptor;
+import todo7.BE.utils.AuthCheckInterceptor;
+import todo7.BE.utils.JwtUtils;
 
 @Configuration
 @EnableWebMvc
 public class CommonWebConfig implements WebMvcConfigurer {
     
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authCheckInterceptor())
+                .addPathPatterns("/api/**");
+    }
+
     @Bean
     public AuthCheckInterceptor authCheckInterceptor() {
         return new AuthCheckInterceptor();
     }
 
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(authCheckInterceptor())
-                .addPathPatterns("/api/**");
-
+    @Bean
+    public JwtUtils jwtUtils() {
+        return new JwtUtils();
     }
 }
