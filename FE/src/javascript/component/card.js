0e7db204463d9b7ebf685a356bd0e4cd499035b4
hyewon3938 <ehak3@naver.com>
2020-04-14 11:51:30 +0900

    Feat : column 별로 서버에서 받은 데이터를 render 하는 기능 구현
    - main.js 에서 column 클래스의 객체를 생성하고, fetch 받은 데이터를 넣어주면 column 생성자에 있는 init함수에 의해 초기 데이터 값을 렌더링 하는 기능

---
+++ FE/src/javascript/component/card.js
@@ -0,0 +1,22 @@
+class Card {
+  constructor(columnName, cardContent) {
+    this.columnName = columnName;
+    this.cardContent = cardContent;
+    this.cardHTML = `<li class="card">
+        <div class="list-icon"><i class="far fa-newspaper"></i></div>
+        <div class="card-content">
+            <div class="content">${this.cardContent}</div>
+            <div class="writer-message">Added by <span class="writer-id">hyewon3938</span></div>
+        </div>
+        <button class="btn-list-delete"><i class="fas fa-times"></i></button>
+    </li>`;
+    this.makeCard();
+  }
+  makeCard(){
+      const listUL = document.querySelector(".list");
+      listUL.innerHTML += this.cardHTML;
+  }
+
+}
+
+export default Card;
