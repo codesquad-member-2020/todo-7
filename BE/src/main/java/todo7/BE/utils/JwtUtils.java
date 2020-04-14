5a059e422ef85bfddf8c93c99de6c823b0545a8f
suhyunsim <cnc1106@naver.com>
2020-04-14 20:15:43 +0900

    Feat: JWT 토큰 검증 기능 구현 - BE [#24]

--- BE/src/main/java/todo7/BE/utils/JwtUtils.java
+++ BE/src/main/java/todo7/BE/utils/JwtUtils.java
@@ -1,14 +1,18 @@
 package todo7.BE.utils;
 
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import java.security.Key;
 import java.util.HashMap;
 import java.util.Map;
 
 public class JwtUtils {
-    private JwtUtils() {}
+//    private JwtUtils() {}
+
+    private Logger logger = LoggerFactory.getLogger(JwtUtils.class);
+    private static final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
 
     public static String createToken(Long id, String userName) {
         Map<String, Object> headers = new HashMap<>();
@@ -19,13 +23,28 @@ public class JwtUtils {
         payloads.put("id", id);
         payloads.put("aud", userName);
 
-        Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
-
         String jwt = Jwts.builder()
                 .setHeader(headers)
                 .setClaims(payloads)
                 .signWith(key)
                 .compact();
         return jwt;
+    }
+
+    public boolean checkJwt(String jwt) {
+        try {
+            Claims claims = Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(jwt)
+                    .getBody();
+            return true;
+        } catch (ExpiredJwtException exception) {
+            logger.info("토큰 만료");
+            return false;
+        } catch (JwtException e) {
+            logger.info("토큰 변조");
+            return false;
+        }
     }
 }
