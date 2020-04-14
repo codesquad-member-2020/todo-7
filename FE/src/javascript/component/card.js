d7387d4984495849a353a411c6b5a3160e4f1564
랄랄랄 <hyewon3938@naver.com>
2020-04-14 20:30:02 +0900

    Feat : 카드 삭제 기능  - FE [#14]
    - 카드의 X버튼 클릭시 삭제를 확인하는 팝업창 띄우기
    - 팝업창 확인 클릭 시 선택된 카드 삭제
    - 팝업창 취소 클릭 시 그대로 팝업창 종료

--- FE/src/javascript/component/card.js
+++ FE/src/javascript/component/card.js
@@ -1,5 +1,5 @@
 class Card {
-  constructor(columnName,cardContent) {
+  constructor(columnName, cardContent) {
     this.columnName = columnName;
     this.cardContent = cardContent;
     this.cardHTML = `<li class="card">
@@ -8,16 +8,40 @@ class Card {
             <div class="content">${this.cardContent}</div>
             <div class="writer-message">Added by <span class="writer-id">hyewon3938</span></div>
         </div>
-        <button class="btn-list-delete"><i class="fas fa-times"></i></button>
+        <button class="btn-list-delete" id="btn-list-delete-${this.cardContent}"><i class="fas fa-times"></i></button>
     </li>`;
     this.makeCard();
+    this.registerEventListener();
   }
-  makeCard(){
-      const listULID = `list-${this.columnName}`;
-      const listUL = document.getElementById(listULID);
-      listUL.insertAdjacentHTML("afterbegin", this.cardHTML);
+  registerEventListener() {
+    const btnDelete = document.getElementById(`btn-list-delete-${this.cardContent}`);
+    btnDelete.addEventListener("click", () => {
+      this.btnDeleteClickHandler();
+    });
   }
-
+  makeCard() {
+    const listULID = `list-${this.columnName}`;
+    const listUL = document.getElementById(listULID);
+    listUL.insertAdjacentHTML("afterbegin", this.cardHTML);
+  }
+  btnDeleteClickHandler() {
+    if (confirm("선택하신 카드를 삭제하시겠습니까?")) {
+      this.removeCard();
+      this.renderCardTotal();
+    } else {
+      return;
+    }
+  }
+  removeCard(){
+    const btnDelete = document.getElementById(`btn-list-delete-${this.cardContent}`);
+    const targetCard = btnDelete.closest(".card");
+    targetCard.remove();
+  }
+  renderCardTotal() {
+    const listTotal = document.getElementById(`list-total-${this.columnName}`);
+    const listUL = document.getElementById(`list-${this.columnName}`);
+    listTotal.innerHTML = listUL.childElementCount;
+  }
 }
 
 export default Card;
