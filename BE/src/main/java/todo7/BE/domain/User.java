f852f99d246fcd9c231879ce40f344bfec96c5dd
suhyunsim <cnc1106@naver.com>
2020-04-16 17:54:30 +0900

    Feat: User와 관련된 기능 구현
    User Repository를 구현하고 User 테이블에 더미 유저 값을 넣는다.

---
+++ BE/src/main/java/todo7/BE/domain/User.java
@@ -0,0 +1,17 @@
+package todo7.BE.domain;
+
+import org.springframework.data.annotation.Id;
+
+public class User {
+    @Id
+    private int id;
+
+    private String userName;
+
+    private String password;
+
+    public User(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+    }
+}
---
+++ BE/src/main/java/todo7/BE/domain/UserRepository.java
@@ -0,0 +1,6 @@
+package todo7.BE.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+}
