d79fee2623284c07b4d3ae8ba3848531a10537e0
suhyunsim <cnc1106@naver.com>
2020-04-16 23:03:01 +0900

    Refactor: 인터셉터 CORS preflight Issue 수정 - BE [#47]
    JWT를 체크하는 부분에서 request method가 OPTIONS일 경우 JWT를 체크하지 않도록 설정한다.

--- BE/src/main/java/todo7/BE/web/AuthCheckInterceptor.java
+++ BE/src/main/java/todo7/BE/web/AuthCheckInterceptor.java
@@ -1,20 +1,32 @@
 package todo7.BE.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.HandlerInterceptor;
 import todo7.BE.utils.JwtUtils;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
 public class AuthCheckInterceptor implements HandlerInterceptor {
-
-    private static final String HEADER_AUTH = "Authorization";
+    private final String HEADER_AUTH = "Authorization";
+    private Logger logger = LoggerFactory.getLogger(AuthCheckInterceptor.class);
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        String token = request.getHeader(HEADER_AUTH);
 
-        if (token != null && JwtUtils.checkJwt(token));
+        logger.info("preHandle Interceptor");
+        logger.info("method : {}", request.getMethod());
+        logger.info("method type : {}", request.getMethod().getClass());
+
+        if (request.getMethod().equals("OPTIONS")) {
+            logger.info("options 메서드는 통과");
+            return true;
+        }
+        String token = request.getHeader(HEADER_AUTH);
+        JwtUtils.checkJwt(token);
+
         return true;
     }
 }
