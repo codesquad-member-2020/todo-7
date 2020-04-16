e8a964e20cd08d267462bfe6eafd81bb378af27f
GangWoon <gangwoon2018@gmail.com>
2020-04-17 00:27:16 +0900

    refactor: delegate 객체 삭제
    viewController에서 delegate를 처리하도록 변경했습니다.
    노티를 받아서 섹션 헤더를 갱신하도록 변경했습니다. 이를 처리하기 위해서 섹션 헤더는 지역변수로 설정했습니다.

--- iOS/ToDo/ToDo/ContainerViewController/ContainerTableViewController.swift
+++ iOS/ToDo/ToDo/ContainerViewController/ContainerTableViewController.swift
@@ -11,22 +11,45 @@ import UIKit
 class ContainerTableViewController: UITableViewController {
     
     // MARK: - Properties
+    private var sectionView: ContainerTableSectionHeaderView!
     
     // MARK: - Lifecycles
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableView
-            .register(ContainerTableSectionHeaderView.self,
-                      forHeaderFooterViewReuseIdentifier: ContainerTableSectionHeaderView.identifier)
+        sectionView = ContainerTableSectionHeaderView(reuseIdentifier: ContainerTableSectionHeaderView.identifier)
         NotificationCenter.default
             .addObserver(self,
-                         selector: #selector(modalNewCardViewController),
+                         selector: #selector(modalNewCard),
                          name: Notification.Name.newCard,
+                         object: nil)
+        NotificationCenter.default
+            .addObserver(self,
+                         selector: #selector(updateCount),
+                         name: Notification.Name.pushCellCount,
                          object: nil)
     }
     
-    @objc func modalNewCardViewController() {
-        let newCardViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(identifier: "NewCardViewController")
-        present(newCardViewController, animated: true, completion: nil)
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        return sectionView
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return ContainerTableSectionHeaderView.height
+    }
+    
+    func updateData(title: String, count: Int) {
+        sectionView.updateCountingLabel(String(count), title)
+    }
+    
+    @objc func modalNewCard() {
+        let viewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(identifier: "NewCardViewController")
+        present(viewController, animated: true, completion: nil)
+    }
+    
+    @objc func updateCount(_ notification: Notification) {
+        guard let userInfo = notification.userInfo?["count"] else { return }
+        let result = userInfo as! (identifier: String, count: String)
+        result.identifier == tableView.description ?
+            sectionView.applyCountChange(result.count) : nil
     }
 }
