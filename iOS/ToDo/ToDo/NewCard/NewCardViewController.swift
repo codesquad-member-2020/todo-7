b5c7fc56c7fb2facbae2ccce3f65b17b3bf1e2ad
GangWoon <gangwoon2018@gmail.com>
2020-04-17 12:10:01 +0900

    feat: 카드 추가 구현 iOS
    컨테이너 뷰를 식별하기 위해서 category.id를 사용해서 구별했고, 새로운 카드 일부만 서버에 전송하도록 했습니다.
    카드를 옮길때 card id가 필요해서 전체 데이터는 다시 받아와야하기 떄문에 그랬습니다.

--- iOS/ToDo/ToDo/NewCard/NewCardViewController.swift
+++ iOS/ToDo/ToDo/NewCard/NewCardViewController.swift
@@ -11,15 +11,19 @@ import UIKit
 class NewCardViewController: UIViewController {
     
     // MARK: - IBOutlets
+    @IBOutlet weak var titleTextView: UITextView!
     @IBOutlet weak var descriptionTextView: UITextView!
     
     // MARK: - Properties
     private var newCardTextViewDelegate: NewCardTextViewDelegate = NewCardTextViewDelegate()
+    private var newCardTitleTextViewDelegate: NewCardTitleTextViewDelegate = NewCardTitleTextViewDelegate()
+    private var categoryId: Int = 0
     
     // MARK: - Lifecycles
     override func viewDidLoad() {
         super.viewDidLoad()
         descriptionTextView.delegate = newCardTextViewDelegate
+        titleTextView.delegate = newCardTitleTextViewDelegate
     }
     
     // MARK: - Actions
@@ -28,11 +32,22 @@ class NewCardViewController: UIViewController {
     }
     
     @IBAction func addCardAction(_ sender: Any) {
+        guard let title = titleTextView.text,
+            let content = descriptionTextView.text else { return }
+        let contents = Contents(title: title, contents: content)
+        Provider.post(categoryId: categoryId, contents: contents)
+        NotificationCenter.default
+            .post(name: Notification.Name.updateCategroy,
+                  object: nil)
         dismiss(animated: true, completion: nil)
     }
     
     @IBAction func tapGesture(_ sender: Any) {
         view.endEditing(true)
     }
+    
     // MARK: - Methods
+    func updateCategoryId(categoryId: Int) {
+        self.categoryId = categoryId
+    }
 }
